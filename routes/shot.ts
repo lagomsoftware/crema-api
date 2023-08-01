527341841a24ab75245f1b091b49ada7e822fd9b
Albin Groen <albin.groen@gmail.com>
2023-08-01 10:46:50 +0200

    Add route for creating shot

--- routes/shot.ts
+++ routes/shot.ts
@@ -1,5 +1,6 @@
 import db from "../lib/prisma";
 import { protectedProcedure, router } from "../lib/trpc";
+import { z } from "zod";
 
 export const shotRouter = router({
   list: protectedProcedure.query(async ({ ctx }) => {
@@ -7,6 +8,30 @@ export const shotRouter = router({
       where: {
         userId: ctx.user.id,
       },
+      orderBy: {
+        createdAt: "desc",
+      },
     });
   }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        notes: z.string().optional(),
+        duration: z.number(),
+        yield: z.number(),
+        dose: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return db.shot.create({
+        data: {
+          ...input,
+          user: {
+            connect: {
+              id: ctx.user.id,
+            },
+          },
+        },
+      });
+    }),
 });
