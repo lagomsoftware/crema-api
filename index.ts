cb0560da90d620675fe7d3382c529c537d986dc7
Albin Groen <albin.groen@gmail.com>
2023-08-28 12:40:47 +0200

    Update email sender and content

--- index.ts
+++ index.ts
@@ -32,9 +32,10 @@ app.post(
 
     await resend.emails.send({
       to: email,
-      from: "noreply@lagom.software",
+      from: "<Crema> noreply@lagom.software",
       subject: "You're on the Cream waitlist!",
-      html: "<p>You are now on the <strong>Crema</strong> waitlist!</p>",
+      text: "You are now on the Crema waitlist! We'll get back to you when we are ready to start taking on beta testers :)",
+      html: "<p>You are now on the <strong>Crema</strong> waitlist! We'll get back to you when we are ready to start taking on beta testers :)</p>",
     });
 
     const eASignup = await db.eASignup.create({
